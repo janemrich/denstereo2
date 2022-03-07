3ada725909812610ab8891c5b49aac7f01eeefa6
Jan Emrich <jan.emrich@igd.fraunhofer.de>
2022-03-07 21:17:48 +0100

    train scenes only to 45

--- core/gdrn_selfocc_modeling/datasets/denstereo_pbr.py
+++ core/gdrn_selfocc_modeling/datasets/denstereo_pbr.py
@@ -64,7 +64,7 @@ class DENSTEREO_PBR_Dataset:
         self.obj2label = OrderedDict((obj, obj_id) for obj_id, obj in enumerate(self.objs))
         ##########################################################
 
-        self.scenes = [f"{i:06d}" for i in range(50)]
+        self.scenes = [f"{i:06d}" for i in ref.denstereo.train_pbr_scenes]
 
     def __call__(self):
         """Load light-weight instance annotations of all images into a list of
