ef0a0139d5a83dc81538c05e97015c823b1ac1f4
Jan Emrich <jan.emrich@igd.fraunhofer.de>
2022-03-08 11:05:38 +0100

    inc batchsize

--- configs/gdrn_selfocc/denstereo/gdrn_selfocc_multistep_denstereo.py
+++ configs/gdrn_selfocc/denstereo/gdrn_selfocc_multistep_denstereo.py
@@ -26,7 +26,7 @@ INPUT = dict(
 )
 
 SOLVER = dict(
-    IMS_PER_BATCH=24,
+    IMS_PER_BATCH=48,
     TOTAL_EPOCHS=2,
     LR_SCHEDULER_NAME="flat_and_anneal",
     ANNEAL_METHOD="cosine",  # "cosine"
