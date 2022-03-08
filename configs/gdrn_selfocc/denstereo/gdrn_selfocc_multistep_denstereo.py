414981be4e9fdde0891bd560eb424916c30d25e9
Jan Emrich <jan.emrich@igd.fraunhofer.de>
2022-03-08 11:39:31 +0100

    BS, lr

--- configs/gdrn_selfocc/denstereo/gdrn_selfocc_multistep_denstereo.py
+++ configs/gdrn_selfocc/denstereo/gdrn_selfocc_multistep_denstereo.py
@@ -26,13 +26,13 @@ INPUT = dict(
 )
 
 SOLVER = dict(
-    IMS_PER_BATCH=96,
+    IMS_PER_BATCH=196,
     TOTAL_EPOCHS=2,
     LR_SCHEDULER_NAME="flat_and_anneal",
     ANNEAL_METHOD="cosine",  # "cosine"
     ANNEAL_POINT=0.72,
     # REL_STEPS=(0.3125, 0.625, 0.9375),
-    OPTIMIZER_CFG=dict(_delete_=True, type="Ranger", lr=1e-4, weight_decay=0),
+    OPTIMIZER_CFG=dict(_delete_=True, type="Ranger", lr=3e-4, weight_decay=0),
     WEIGHT_DECAY=0.0,
     WARMUP_FACTOR=0.001,
     WARMUP_ITERS=1000,
