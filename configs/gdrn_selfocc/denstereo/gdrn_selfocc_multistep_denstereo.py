279da368f8ac30c28a4aab6af41c5b166b35e9cf
Jan Emrich <jan.emrich@igd.fraunhofer.de>
2022-03-10 21:13:37 +0100

    240 epochs

--- configs/gdrn_selfocc/denstereo/gdrn_selfocc_multistep_denstereo.py
+++ configs/gdrn_selfocc/denstereo/gdrn_selfocc_multistep_denstereo.py
@@ -27,7 +27,7 @@ INPUT = dict(
 
 SOLVER = dict(
     IMS_PER_BATCH=225,
-    TOTAL_EPOCHS=120,
+    TOTAL_EPOCHS=240,
     LR_SCHEDULER_NAME="flat_and_anneal",
     ANNEAL_METHOD="cosine",  # "cosine"
     ANNEAL_POINT=0.72,
