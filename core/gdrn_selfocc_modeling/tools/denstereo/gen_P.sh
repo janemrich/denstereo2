c5cab9854e1f2f46c3108adfa4407938adfd7f01
Jan Emrich <jan.emrich@igd.fraunhofer.de>
2022-03-11 16:21:23 +0000

    gen_p.sh

--- core/gdrn_selfocc_modeling/tools/denstereo/gen_P.sh
+++ core/gdrn_selfocc_modeling/tools/denstereo/gen_P.sh
@@ -1,1 +1,6 @@
-a2 --cpus-per-task 55 --mem-per-cpu 1500M python -m core.gdrn_selfocc_modeling.tools.denstereo.generate_pbr_P --dataset denstereo --split train_pbr_right --threads 50
+srun -w ampere2 --cpus-per-task 55 --mem-per-cpu 1500M \
+    python -m core.gdrn_selfocc_modeling.tools.denstereo.generate_pbr_P \
+        --dataset denstereo --split train_pbr_left --threads 50 &
+srun -w ampere2 --cpus-per-task 55 --mem-per-cpu 1500M \
+    python -m core.gdrn_selfocc_modeling.tools.denstereo.generate_pbr_P \
+        --dataset denstereo --split train_pbr_right --threads 50
