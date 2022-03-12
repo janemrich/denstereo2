66b51240bcce1b12abd564ffedc78d4233a5c5cf
Jan Emrich <jan.emrich@igd.fraunhofer.de>
2022-03-12 09:34:07 +0000

    npz load fixes

--- core/gdrn_modeling/data_loader.py
+++ core/gdrn_modeling/data_loader.py
@@ -461,10 +461,7 @@ class GDRN_DatasetFromList(Base_DatasetFromList):
         dataset_dict["roi_extent"] = torch.tensor(roi_extent, dtype=torch.float32)
 
         # load xyz =======================================================
-        try:
-            xyz_info = np.load(inst_infos["xyz_path"])
-        except FileNotFoundError:
-            xyz_info = mmcv.load(inst_infos["xyz_path"])
+        xyz_info = np.load(inst_infos["xyz_path"]) # also load npz files
         x1, y1, x2, y2 = xyz_info["xyxy"]
         # float16 does not affect performance (classification/regresion)
         xyz_crop = xyz_info["xyz_crop"]
