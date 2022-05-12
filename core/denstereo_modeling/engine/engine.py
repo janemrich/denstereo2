f5009120a9e7b69e9e158a6bd8df14f9b435323d
Jan Emrich <jan.emrich@igd.fraunhofer.de>
2022-05-12 22:27:42 +0200

    engine reduce debug

--- core/denstereo_modeling/engine/engine.py
+++ core/denstereo_modeling/engine/engine.py
@@ -406,10 +406,12 @@ def do_train(cfg, args, model, optimizer, renderer=None, resume=False):
             print(loss_dict, flush=True)
             for k in loss_dict.keys():
                 print(k, loss_dict[k].shape, loss_dict[k], flush=True)
-            loss_dict_reduced = {k: v.item() for k, v in comm.reduce_dict(loss_dict).items()}
-            losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+            # loss_dict_reduced = {k: v.item() for k, v in comm.reduce_dict(loss_dict).items()}
+            # losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+            losses_reduced = sum(loss for loss in loss_dict.values())
             if comm.is_main_process():
-                storage.put_scalars(total_loss=losses_reduced, **loss_dict_reduced)
+                # storage.put_scalars(total_loss=losses_reduced, **loss_dict_reduced)
+                storage.put_scalars(total_loss=losses_reduced, **loss_dict)
 
             # backward & optimize ======================================================
             if AMP_ON:
