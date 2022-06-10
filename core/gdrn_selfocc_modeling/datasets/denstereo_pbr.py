ed77da10a1a0ea7a3eafdd1dc0970246263d181b
Jan Emrich <jan.emrich@igd.fraunhofer.de>
2022-06-10 19:11:04 +0200

    not use link again

--- core/gdrn_selfocc_modeling/datasets/denstereo_pbr.py
+++ core/gdrn_selfocc_modeling/datasets/denstereo_pbr.py
@@ -238,9 +238,7 @@ class DENSTEREO_PBR_Dataset:
             dataset_dicts = dataset_dicts[: self.num_to_load]
         logger.info("loaded {} dataset dicts, using {}s".format(len(dataset_dicts), time.perf_counter() - t_start))
 
-        print(cache_path)
-        if not osp.isfile(osp.dirname(cache_path)):
-            mmcv.mkdir_or_exist(osp.dirname(cache_path))
+        mmcv.mkdir_or_exist(osp.dirname(cache_path))
         mmcv.dump(dataset_dicts, cache_path, protocol=4)
         logger.info("Dumped dataset_dicts to {}".format(cache_path))
         return dataset_dicts
