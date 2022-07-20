8004c6961b3dd82a1cc552c9deb56d43187ab162
Jan Emrich <jan.emrich@igd.fraunhofer.de>
2022-07-21 00:57:09 +0200

    disp early

--- configs/denstereo/denstereo/denstereo_scissor_disp_early.py
+++ configs/denstereo/denstereo/denstereo_scissor_disp_early.py
@@ -26,12 +26,18 @@ SOLVER = dict(
     ANNEAL_METHOD="cosine",  # "cosine"
     ANNEAL_POINT=0.72,
     # REL_STEPS=(0.3125, 0.625, 0.9375),
-    OPTIMIZER_CFG=dict(_delete_=True, type="Ranger", lr=1e-4, weight_decay=0),
+    OPTIMIZER_CFG=dict(_delete_=True, type="Ranger", lr=1e-3, weight_decay=0),
     WEIGHT_DECAY=0.0,
     WARMUP_FACTOR=0.001,
     WARMUP_ITERS=1000,
     # WARMUP_ITERS=1,
     CHECKPOINT_PERIOD=10,
+)
+
+DATALOADER = dict(
+    # Number of data loading threads
+    NUM_WORKERS=10,
+    # FILTER_VISIB_THR=0.1,
 )
 
 DATASETS = dict(
