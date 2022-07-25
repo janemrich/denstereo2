f2b4d3b24cd53b68a3d194732b0874606dc71f62
Jan Emrich <jan.emrich@igd.fraunhofer.de>
2022-07-25 21:30:16 +0000

    train scripts

--- core/denstereo_modeling/train_gdrn_slurm.sh
+++ core/denstereo_modeling/train_gdrn_slurm.sh
@@ -1,25 +1,41 @@
+#!/bin/sh
 #!/usr/bin/env bash
 set -x
+
+echo $HOSTNAME
+
 # commonly used opts:
 
 # MODEL.WEIGHTS: resume or pretrained, or test checkpoint
 CFG=$1
 NGPU=$2
 
-#NCCL_DEBUG=INFO
-#OMP_NUM_THREADS=1
-#MKL_NUM_THREADS=1
+NCCL_DEBUG=INFO
+OMP_NUM_THREADS=1
+MKL_NUM_THREADS=1
 
-cd /opt/cache/
-if [ -d denStereo-SO ] ; then
-	git clone git@github.com:janemrich/denStereo-SO.git
-fi
+export RANK=0
+export WORLD_SIZE=1
+
+BASE_PATH=/opt/spool/jemrich-denstereo
+
+mkdir $BASE_PATH
+cd $BASE_PATH
+#if [ -d denStereo-SO ] ; then
+git clone git@github.com:janemrich/denStereo-SO.git
+#fi
 cd denStereo-SO
 git checkout denstereo
 git pull
 ln -s /opt/spool/jemrich datasets
+rsync -aP pc3002:/opt/datasets/jemrich/cache/ .cache
+rsync -aP pc3002:/opt/datasets/jemrich/VOCdevkit datasets
 
-echo train_gdrn.sh: python core/gdrn_selffocc_modeling/main_gdrn.py --config-file $CFG --num-gpus $NGPU ${@:3}
+echo train_gdrn.sh: python core/denstereo_modeling/main_gdrn.py --config-file $CFG --num-gpus $NGPU ${@:3}
 
 python core/denstereo_modeling/main_gdrn.py \
-    --config-file $CFG --num-gpus $NGPU  ${@:3}
+    --config-file $CFG --num-gpus $NGPU  ${@:3} #--launcher dataparallel
+
+# sync output
+rsync -aP $BASE_PATH/denStereo-SO/output pc3002:/opt/datasets/jemrich/
+rsync -aP $BASE_PATH/denStereo-SO/.cache/ pc3002:/opt/datasets/jemrich/cache
--- core/gdrn_selfocc_modeling/train_gdrn_slurm.sh
+++ core/gdrn_selfocc_modeling/train_gdrn_slurm.sh
@@ -10,16 +10,25 @@ NGPU=$2
 #OMP_NUM_THREADS=1
 #MKL_NUM_THREADS=1
 
-cd /opt/cache/
-if [ -d denStereo-SO ] ; then
-	git clone git@github.com:janemrich/denStereo-SO.git
-fi
+BASE_PATH=/opt/spool/jemrich-denstereo
+
+mkdir $BASE_PATH
+cd $BASE_PATH
+
+git clone git@github.com:janemrich/denStereo-SO.git
 cd denStereo-SO
 git checkout denstereo
 git pull
 ln -s /opt/spool/jemrich datasets
 
+rsync -aP pc3002:/opt/datasets/jemrich/cache/ .cache 
+rsync -aP pc3002:/opt/datasets/jemrich/VOCdevkit datasets
+
 echo train_gdrn.sh: python core/gdrn_selffocc_modeling/main_gdrn.py --config-file $CFG --num-gpus $NGPU ${@:3}
 
 python core/gdrn_selfocc_modeling/main_gdrn.py \
     --config-file $CFG --num-gpus $NGPU  ${@:3}
+
+# sync output
+rsync -aP $BASE_PATH/denStereo-SO/output pc3002:/opt/datasets/jemrich/
+rsync -aP $BASE_PATH/denStereo-SO/.cache/ pc3002:/opt/datasets/jemrich/cache
