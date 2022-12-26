f01742e8cf0b15b0932c4819c141c0a94d9a7d4c
Jan <mail@janemrich.de>
2022-12-26 22:06:26 +0000

    container runner improved

--- run_gdrn_container.sh
+++ run_gdrn_container.sh
@@ -7,60 +7,58 @@ EVAL=$5
 
 
 ### setup environment on spool
-BASE_PATH="/opt/spool/jemrich-denstereo"
 DATASET_DICT_CACHE="/opt/cache/jemrich/datadicts"
+OUTPUT_PATH="/opt/spool/jemrich-denstereo/output"
 IMAGE_CACHE="/opt/cache/jemrich/image.tar"
 
-if [ ! -d $BASE_PATH ]; then
-    mkdir $BASE_PATH
-fi
+echo "setup environment on spool"
 
-cd $BASE_PATH
-if [ ! -d $BASE_PATH/denstereo-so ]; then
-    git clone git@git.igd-r.fraunhofer.de:jemrich/denstereo-so.git
+if [ ! -d $OUTPUT_PATH ]; then
+    mkdir $OUTPUT_PATH
 fi
-
-cd denstereo-so
-git checkout denstereo
-git pull
 
 ### create dataset dict cache
 if [ ! -d $DATASET_DICT_CACHE ]; then
     mkdir -p $DATASET_DICT_CACHE
 fi
 
-### share cached dataset dicts
+### sync cached dataset dicts
 echo ""
 echo "sync dataset dict cache from pc3002"
 rsync -aP pc3002:/opt/datasets/jemrich/cache/ $DATASET_DICT_CACHE 
 
-EVALUATE = ""
+EVALUATE=""
 if [ $EVAL == "True" ]; then
     echo ""
     echo "sync eval model from pc3002"
-    rsync -a --update --whole-file pc3002:/opt/datasets/jemrich/output/${METHOD}/${DATASET}/${RUN}/model_final.pth output/${METHOD}/${DATASET}/${RUN}/model_final.pth
+    rsync -aP --update --whole-file pc3002:/opt/datasets/jemrich/output/${METHOD}/${DATASET}/${RUN}/model_final.pth ${OUTPUT_PATH}/${METHOD}/${DATASET}/${RUN}/model_final.pth
     EVALUATE="--evaluate"
 fi
 
-source rootless_docker_env.sh
+source rootless_docker_env.sh share-data
 
 ### cache and load docker image
 if [ ! -f $IMAGE_CACHE.gz ]; then
     mkdir -p "/opt/cache/jemrich"
 fi
 echo "sync image from pc3002"
-rsync -a --update --whole-file pc3002:~/den-env-image.tar.gz $IMAGE_CACHE.gz
+rsync -aP --update --whole-file pc3002:~/den-env-image.tar.gz $IMAGE_CACHE.gz
 
-if [ ! -f $IMAGE_CACHE ]; then
+if [[ $IMAGE_CACHE.gz -nt $IMAGE_CACHE ]]; then
+    echo "newer image found on pc3002"
     echo "unzip image..."
-    gzip -dv $IMAGE_CACHE.gz
+    gzip -dv --force $IMAGE_CACHE.gz
 fi
 
 echo "load image..."
+docker ps
+docker images 
 docker load -i $IMAGE_CACHE
 
-docker run --shm-size=50G --rm --gpus $NGPUS -it -v $BASE_PATH/denstereo-so:/denstereo-so -v /opt/spool/jemrich/:/denstereo-so/datasets -v $DATASET_DICT_CACHE:/denstereo-so/.cache denstereo-env:latest "cd /denstereo-so; python launch_main.py $RUN $EVALUATE"
+docker run --shm-size=50G --rm --name prod --gpus $NGPUS -dit -v /denstereo -v $OUTPUT_PATH:/denstereo/output -v ~/denstereo-so/runs:/denstereo/runs -v /opt/spool/jemrich/:/denstereo/datasets -v $DATASET_DICT_CACHE:/denstereo/.cache denstereo-env:latest
+docker exec -it prod sh -c "cd /denstereo; git clone git@git.igd-r.fraunhofer.de:jemrich/denstereo-so.git; cd denstereo-so; git checkout denstereo; ln -s ../.cache .cache; ln -s ../runs runs; ln -s ../datasets datasets; rm output/.gitignore; rmdir output; ln -s ../output output; python launch_main.py $RUN $EVALUATE"
+docker stop prod
 
 echo "sync output..."
-rsync -aP $BASE_PATH/denstereo-so/output pc3002:/opt/datasets/jemrich/
+rsync -aP $OUTPUT_PATH/ pc3002:/opt/datasets/jemrich/output
 rsync -aP $DATASET_DICT_CACHE/ pc3002:/opt/datasets/jemrich/cache
