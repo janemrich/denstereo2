9fdd81f3fbcdc89e51c87d845dfa2c50240b0986
Jan <mail@janemrich.de>
2022-12-26 22:44:02 +0000

    launch main with timestamp

--- launch_main.py
+++ launch_main.py
@@ -4,12 +4,14 @@ import subprocess
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Train or evaluate a gdrn model")
-    parser.add_argument("run", help="unique run id", type=str)
+    parser.add_argument("run", help="config file name", type=str)
+    parser.add_argument("run_id", help="unique run id", type=str)
     parser.add_argument("--evaluate", action="store_true", default=False, help="evaluate or train")
     parser.add_argument("--checkpoint", help="checkpoint to load", type=str)
 
     args = parser.parse_args()
-    run = args.run
+    run = args.config
+    run_id = args.run_id
 
     with open('runs/{run}.yaml'.format(run=run), 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
@@ -28,7 +30,7 @@ if __name__ == '__main__':
     )
 
     if args.evaluate:
-        weights = 'output/{method}/{dataset}/{run}/model_final.pth'.format(
+        weights = 'output/{method}/{dataset}/{run_id}/model_final.pth'.format(
             method=method,
             dataset=dataset,
             run=run
@@ -39,7 +41,7 @@ if __name__ == '__main__':
         print('Evaluating model: {weights}'.format(weights=weights))
 
 
-    s = "python core/{method}_modeling/main_gdrn.py --config-file {config} --num-gpus {gpus} {eval} --opts SOLVER.IMS_PER_BATCH={bs} SOLVER.TOTAL_EPOCHS={epochs} OUTPUT_DIR=\"output/{method}/{dataset}/{run}\" SOLVER.MAX_TO_KEEP={max_to_keep} SOLVER.CHECKPOINT_PERIOD={checkpoint_period} {weights}"
+    s = "python core/{method}_modeling/main_gdrn.py --config-file {config} --num-gpus {gpus} {eval} --opts SOLVER.IMS_PER_BATCH={bs} SOLVER.TOTAL_EPOCHS={epochs} OUTPUT_DIR=\"output/{method}/{dataset}/{run_id}\" SOLVER.MAX_TO_KEEP={max_to_keep} SOLVER.CHECKPOINT_PERIOD={checkpoint_period} {weights}"
     s = s.format(
         method=method,
         dataset=dataset,
