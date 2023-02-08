694094fced278a092844693ed446a72bd89da018
Jan <mail@janemrich.de>
2023-02-08 02:44:34 +0000

    change lr

--- configs/gdrn_stereo/stereobj_1m/screwdriver_lr-h.py
+++ configs/gdrn_stereo/stereobj_1m/screwdriver_lr-h.py
@@ -38,7 +38,7 @@ SOLVER = dict(
     ANNEAL_METHOD="cosine",  # "cosine"
     ANNEAL_POINT=0.72,
     # REL_STEPS=(0.3125, 0.625, 0.9375),
-    OPTIMIZER_CFG=dict(_delete_=True, type="Ranger", lr=1e-2, weight_decay=0),
+    OPTIMIZER_CFG=dict(_delete_=True, type="Ranger", lr=5e-3, weight_decay=0),
     WEIGHT_DECAY=0.0,
     WARMUP_FACTOR=0.001,
     WARMUP_ITERS=1000,
