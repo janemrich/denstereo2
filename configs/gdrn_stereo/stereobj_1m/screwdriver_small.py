5a4e810ed5e13f6981768e423f9e096e4da14d05
Jan <mail@janemrich.de>
2023-02-20 16:18:21 +0000

    screwdriver small inc lr

--- configs/gdrn_stereo/stereobj_1m/screwdriver_small.py
+++ configs/gdrn_stereo/stereobj_1m/screwdriver_small.py
@@ -41,7 +41,7 @@ SOLVER = dict(
     ANNEAL_METHOD="cosine",  # "cosine"
     ANNEAL_POINT=0.72,
     # REL_STEPS=(0.3125, 0.625, 0.9375),
-    OPTIMIZER_CFG=dict(_delete_=True, type="Ranger", lr=1e-4, weight_decay=0),
+    OPTIMIZER_CFG=dict(_delete_=True, type="Ranger", lr=5e-4, weight_decay=0),
     WEIGHT_DECAY=0.0,
     WARMUP_FACTOR=0.001,
     WARMUP_ITERS=1000,
